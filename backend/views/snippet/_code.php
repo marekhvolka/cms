12bab27a87473cd1ef05e4229b85b341b30ed3cc
Peto <peterkapusta22@gmail.com>
2016-04-22 13:13:00 +0200

    Snippet vars - redoing codes (dynamic form widget removing)

--- backend/views/snippet/_code.php
+++ backend/views/snippet/_code.php
@@ -1,8 +1,70 @@
-<?php
+<?php 
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+use yii\helpers\Html;
+use conquer\codemirror\CodemirrorAsset;
+use conquer\codemirror\CodemirrorWidget;
 
+?>
+
+<div class="item panel panel-default"><!-- widgetBody -->
+    <button type="button" class="remove-item btn btn-danger btn-xs">
+        <i class="glyphicon glyphicon-minus"></i>
+    </button>
+
+    <div class="panel-heading"> 
+        <div class="input-group">
+            <?= $form->field($modelSnippetCode, "[{$i}]name")->textInput(['maxlength' => true]) ?>
+            <div class="pull-right">
+                <button type="button" class="add-item btn btn-success btn-xs">
+                    <i class="glyphicon glyphicon-plus"></i>
+                </button>
+            </div>
+            <div class="clearfix"></div>
+        </div>
+    </div>
+    <div class="panel-body">
+        <?php
+        // necessary for update action.
+        if (!$modelSnippetCode->isNewRecord) {
+            echo Html::activeHiddenInput($modelSnippetCode, "[{$i}]id");
+        }
+        ?>
+
+        <div class="row">
+            <div class="col-sm-12">
+                <?php
+                echo $form->field($modelSnippetCode, "[{$i}]code")->widget(
+                        CodemirrorWidget::className(), [
+                    'assets' => [
+                        CodemirrorAsset::MODE_CLIKE,
+                        CodemirrorAsset::KEYMAP_EMACS,
+                        CodemirrorAsset::ADDON_EDIT_MATCHBRACKETS,
+                        CodemirrorAsset::ADDON_COMMENT,
+                        CodemirrorAsset::ADDON_DIALOG,
+                        CodemirrorAsset::ADDON_SEARCHCURSOR,
+                        CodemirrorAsset::ADDON_SEARCH,
+                    ],
+                    'settings' => [
+                        'lineNumbers' => true,
+                        'mode' => 'text/x-csrc',
+                    ],
+                    'options' => [
+                        'class' => 'html-editor'
+                    ]
+                        ]
+                );
+                ?>
+            </div>
+        </div><!-- .row -->
+        <div class="row">
+            <div class="col-sm-12">
+                <?= $form->field($modelSnippetCode, "[{$i}]popis")->textarea(['rows' => '4']) ?>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-12">
+                <?= $form->field($modelSnippetCode, "[{$i}]portal")->textInput(['maxlength' => true]) ?>
+            </div>
+        </div>
+    </div>
+</div>
--- backend/views/snippet/_form.php
+++ backend/views/snippet/_form.php
@@ -39,94 +39,11 @@ use yii\web\View;
         <div class="panel panel-default">
             <div class="panel-heading"><h4><i class="glyphicon glyphicon-envelope"></i> Alternatívy</h4></div>
             <div class="panel-body">
-                <?php DynamicFormWidget::begin([
-                    'widgetContainer' => 'dynamicform_wrapper', // required: only alphanumeric characters plus "_" [A-Za-z0-9_]
-                    'widgetBody' => '.container-items', // required: css class selector
-                    'widgetItem' => '.item', // required: css class
-                    'min' => 1, // 0 or 1 (default 1)
-                    'insertButton' => '.add-item', // css class
-                    'deleteButton' => '.remove-item', // css class
-                    'model' => $modelsSnippetCode[0],
-                    'formId' => 'dynamic-form',
-                    'formFields' => [
-                        'full_name',
-                        'address_line1',
-                        'address_line2',
-                        'city',
-                        'state',
-                        'postal_code',
-                    ],
-                ]); ?>
-
-                <div class="container-items"><!-- widgetContainer -->
+                <div class="container-items">
                     <?php foreach ($modelsSnippetCode as $i => $modelSnippetCode): ?>
-                        <div class="item panel panel-default"><!-- widgetBody -->
-                            <button type="button" class="remove-item btn btn-danger btn-xs">
-                                <i class="glyphicon glyphicon-minus"></i>
-                            </button>
-                            
-                            <div class="panel-heading"> 
-                                <div class="input-group">
-                                    <?= $form->field($modelSnippetCode, "[{$i}]name")->textInput(['maxlength' => true]) ?>
-                                    <div class="pull-right">
-                                        <button type="button" class="add-item btn btn-success btn-xs">
-                                            <i class="glyphicon glyphicon-plus"></i>
-                                        </button>
-                                        
-                                    </div>
-                                    <div class="clearfix"></div>
-                                </div>
-                            </div>
-                            <div class="panel-body">
-                                <?php
-                                // necessary for update action.
-                                if (! $modelSnippetCode->isNewRecord) {
-                                    echo Html::activeHiddenInput($modelSnippetCode, "[{$i}]id");
-                                }
-                                ?>
-
-                                <div class="row">
-                                    <div class="col-sm-12">
-                                        <?php
-                                            echo $form->field($modelSnippetCode, "[{$i}]code")->widget(
-                                                CodemirrorWidget::className(),
-                                                [
-                                                    'assets' => [
-                                                        CodemirrorAsset::MODE_CLIKE,
-                                                        CodemirrorAsset::KEYMAP_EMACS,
-                                                        CodemirrorAsset::ADDON_EDIT_MATCHBRACKETS,
-                                                        CodemirrorAsset::ADDON_COMMENT,
-                                                        CodemirrorAsset::ADDON_DIALOG,
-                                                        CodemirrorAsset::ADDON_SEARCHCURSOR,
-                                                        CodemirrorAsset::ADDON_SEARCH,
-                                                    ],
-                                                    'settings' => [
-                                                        'lineNumbers' => true,
-                                                        'mode' => 'text/x-csrc',
-                                                    ],
-                                                    'options' => [
-                                                        'class' => 'html-editor'
-                                                    ]
-                                                ]
-                                            );
-                                        ?>
-                                    </div>
-                                </div><!-- .row -->
-                                <div class="row">
-                                    <div class="col-sm-12">
-                                        <?= $form->field($modelSnippetCode, "[{$i}]popis")->textarea(['rows' => '4']) ?>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-sm-12">
-                                        <?= $form->field($modelSnippetCode, "[{$i}]portal")->textInput(['maxlength' => true]) ?>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    <?php endforeach; ?>
+                    <?= $this->render('_code', ['modelSnippetCode' => $modelSnippetCode, 'i' => $i, 'form' => $form]) ;?>
+                    <?php endforeach;?>
                 </div>
-                <?php DynamicFormWidget::end(); ?>
             </div>
         </div>
     </div>
