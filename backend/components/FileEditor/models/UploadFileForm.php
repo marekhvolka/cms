7e8d71c5cefd8402965afef0ba3ed99c648e6681
Marek <marek.hvolka@gmail.com>
2016-06-06 18:53:41 +0200

    FileEditor moved to backend/components

--- backend/components/FileEditor/models/UploadFileForm.php
+++ backend/components/FileEditor/models/UploadFileForm.php
@@ -2,25 +2,35 @@
 
 namespace backend\components\FileEditor\models;
 
-use backend\components\FileEditor\FileEditorWidget;
+use backend\components\PathHelper;
 use yii\base\Model;
 use yii\web\UploadedFile;
 
+/**
+ * Used to upload a new file.
+ *
+ * @package common\widgets\FileEditor\models
+ */
 class UploadFileForm extends Model
 {
     /**
+     * The directory into which the uploaded file should be put in.
      * @var string
      */
     public $directory;
     /**
+     * The uploaded file.
+     *
      * @var UploadedFile
      */
     public $file;
 
     /**
+     * The base / root directory to the file be put in.
+     *
      * @var string
      */
-    private $baseDir = '';
+    private $baseDir;
 
     /**
      * UploadFileForm constructor.
@@ -33,31 +43,40 @@ class UploadFileForm extends Model
     }
 
 
+    /**
+     * @inheritdoc
+     */
     public function rules()
     {
         return [
             [['file'], 'file', 'skipOnEmpty' => false],
-            ['directory', function ($attribute, $parameters) {
-                $realpath = '/' . FileEditorWidget::normalizePath($this->baseDir . DIRECTORY_SEPARATOR . trim($this->$attribute, '/'));
+            ['directory', function ($attribute) {
+                $realpath = '/' . PathHelper::normalizePath($this->baseDir . DIRECTORY_SEPARATOR . trim($this->$attribute, '/'));
 
-                if (strrpos($realpath, realpath($this->baseDir), -strlen($realpath)) === false) {
+                if (PathHelper::isInside($realpath, realpath($this->baseDir))) {
                     $this->addError($attribute, 'Invalid directory.');
-                } else {
-                    FileEditorWidget::makePath($realpath);
                 }
             }]
         ];
     }
 
+    /**
+     * If the model is valid (or the $validate param is false), upload the file.
+     *
+     * @param bool $validate check model's validity
+     * @return string the path to which the file is put in
+     */
     public function upload($validate = true)
     {
         if (!$validate || $this->validate()) {
-            $path = $this->baseDir . DIRECTORY_SEPARATOR . $this->directory . DIRECTORY_SEPARATOR .
-                $this->file->baseName . '.' . $this->file->extension;
+            $path = $this->baseDir . DIRECTORY_SEPARATOR . $this->directory;
+
+            PathHelper::makePath(PathHelper::normalizePath($path));
+
+            $path .= DIRECTORY_SEPARATOR . $this->file->baseName . '.' . $this->file->extension;
             $this->file->saveAs($path);
+
             return $path;
-        } else {
-            return false;
         }
     }
 }
--- backend/components/FileEditor/views/file-editor.php
+++ backend/components/FileEditor/views/file-editor.php
@@ -5,22 +5,29 @@ use backend\components\FileEditor\models\EditFileForm;
 use conquer\codemirror\CodemirrorAsset;
 use conquer\codemirror\CodemirrorWidget;
 use kartik\select2\Select2;
+use yii\bootstrap\ActiveForm;
 use yii\bootstrap\Html;
 use yii\helpers\Url;
-use yii\widgets\ActiveForm;
 
 /* @var $this \yii\web\View */
 /* @var $editFileForm EditFileForm */
 /* @var $uploadFileForm EditFileForm */
 /* @var $createDirectoryForm CreateDirectoryForm */
-/* @var $directory string */
-/* @var $fileTree array */
-/* @var $directoryTree array */
-/* @var $isImageLoaded boolean */
+/* @var $directory string directory to display */
+/* @var $fileTree array list of files / subdirectories */
+/* @var $directoryTree array list of directories (as strings with whole relative paths */
+/* @var $isImageLoaded boolean is an image shown by default? */
 
 FileEditorAsset::register($this);
 
-function build_one_level($data, $from_dir = '')
+/**
+ * Displays one level of directory tree and recursively calls itself to display the inner items as trees as well.
+ *
+ * @param $data array directory / files tree... files are simple strings, directories are arrays containing other
+ * items (directory's name is the index key)
+ * @param string $from_dir the tree stored in $data param is subfolder of ...
+ */
+function build_file_tree($data, $from_dir = '')
 {
     ?>
     <ul class="jqueryFileTree">
@@ -30,14 +37,14 @@ function build_one_level($data, $from_dir = '')
                 // directory
                 ?>
                 <li class="directory expanded">
-                    <?= $index ?> <a href="<?= Url::current(['file' => $path,
-                                                                          'fileAction' => 'delete'])
+                    <?= $index ?> <a href="<?= Url::current(['file'       => $path,
+                        'fileAction' => 'delete'])
                     ?>" class="delete">x</a> <a href="#"
                                                 class="add-file"
                                                 data-name='<?= $path ?>'
                                                 data-toggle="modal"
                                                 data-target="#uploadFileModal">+</a>
-                    <?php build_one_level($item, $from_dir . '/' . $index) ?>
+                    <?php build_file_tree($item, $from_dir . '/' . $index) ?>
                 </li>
                 <?php
             } else {
@@ -63,27 +70,24 @@ function build_one_level($data, $from_dir = '')
 
 <?php $this->beginBlock('button'); ?>
 <?= Html::a('Nahrať súbor', "#", ['class'       => 'btn btn-success pull-right', 'data-name' => '/',
-                                  'data-toggle' => "modal", 'data-target' => '#uploadFileModal']) ?>
+    'data-toggle' => "modal", 'data-target' => '#uploadFileModal']) ?>
 
 <?= Html::a('Vytvoriť priečinok', "#", ['class'       => 'btn btn-success pull-right', 'data-name' => '/',
-                                        'data-toggle' => "modal", 'data-target' => '#createDirectoryModal']) ?>
+    'data-toggle' => "modal", 'data-target' => '#createDirectoryModal']) ?>
 <?php $this->endBlock(); ?>
 
 <div class="file-editor">
     <div class="col-xs-12 col-sm-3">
         <div class="row list">
-
-
-            <?php build_one_level($fileTree) ?>
+            <?php build_file_tree($fileTree) ?>
         </div>
     </div>
     <div class="col-xs-12 col-sm-9">
         <div class="row">
-            <h3 class="new-file">Nový súbor</h3>
-            <?php if ($editFileForm->fileName == null) { ?><h3 class="select-a-file">Vyberte súbor alebo pridajte
-                nový</h3><?php } ?>
-
-            <div class="file-editing" <?php if ($isImageLoaded) : ?>style="display: none"<?php endif; ?>>
+            <?php if ($editFileForm->fileName == null) : ?>
+                <h3 class="select-a-file">Vyberte súbor alebo nahrajte nový</h3>
+            <?php endif; ?>
+            <div class="file-editing" <?php if ($isImageLoaded || $editFileForm->fileName == null) : ?>style="display: none"<?php endif; ?>>
                 <?php $form = ActiveForm::begin() ?>
                 <?= CodemirrorWidget::widget([
                         'name'     => 'EditFileForm[text]',
@@ -98,8 +102,7 @@ function build_one_level($data, $from_dir = '')
                             CodemirrorAsset::ADDON_SEARCH,
                         ],
                         'settings' => [
-                            'mode'        => 'application/x-httpd-php',
-                            'readOnly'    => true
+                            'mode'     => 'application/x-httpd-php'
                         ],
                     ]
                 ) ?>
@@ -110,13 +113,16 @@ function build_one_level($data, $from_dir = '')
                 ]) ?>
                 <?php $form->end() ?>
             </div>
-            <div class="image">
-                <img src="<?php if($isImageLoaded) { echo Url::current(['file' => $editFileForm->fileName]); }?>">
+            <div class="image"> <!-- will be shown only if an image is opened -->
+                <img src="<?php if ($isImageLoaded) {
+                    echo Url::current(['file' => $editFileForm->fileName]);
+                } ?>">
             </div>
         </div>
     </div>
 </div>
 
+<!-- UPLOAD A NEW FILE MODAL -->
 <div class="modal fade" id="uploadFileModal" tabindex="-1" role="dialog" aria-labelledby="uploadFileModalLabel">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
@@ -130,9 +136,9 @@ function build_one_level($data, $from_dir = '')
             <div class="modal-body">
                 <?= $form->field($uploadFileForm, 'file')->fileInput() ?>
                 <?= $form->field($uploadFileForm, 'directory')->widget(Select2::className(), [
-                    'data' => $directoryTree,
+                    'data'          => $directoryTree,
                     'maintainOrder' => false,
-                    'hideSearch' => true
+                    'hideSearch'    => true
                 ]) ?>
             </div>
             <div class="modal-footer">
@@ -147,7 +153,9 @@ function build_one_level($data, $from_dir = '')
     </div>
 </div>
 
-<div class="modal fade" id="createDirectoryModal" tabindex="-1" role="dialog" aria-labelledby="createDirectoryModalLabel">
+<!-- CREATE A NEW DIRECTORY MODAL -->
+<div class="modal fade" id="createDirectoryModal" tabindex="-1" role="dialog"
+     aria-labelledby="createDirectoryModalLabel">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <?php $form = ActiveForm::begin() ?>
