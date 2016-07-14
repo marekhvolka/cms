ced1e61f3d910e80d8cd4c94c2d877baed37b495
Marek <marek.hvolka@gmail.com>
2016-07-14 16:50:38 +0200

    Changes in develop top bar

--- frontend/views/site/_top-bar.php
+++ frontend/views/site/_top-bar.php
@@ -1,10 +1,19 @@
 <?php
+use backend\models\Page;
 use backend\models\Portal;
 use yii\bootstrap\Html;
+use yii\helpers\Url;
 
 /** @var $portal Portal */
+/** @var $page Page */
 ?>
 
-<div class="cms-top-bar">
+<div class="cms-top-bar" xmlns:Html="http://www.w3.org/1999/html">
     Prihlásený ako <?= Yii::$app->user->getIdentity()->username ?>.
+
+    <div class="pull-right">
+    <?= Html::a('Upraviť podstránku', '/backend/web/page/edit/' . $page->id) ?>
+    <?= $page->product ?
+        Html::a('Upraviť produkt', '/backend/web/product/edit/' . $page->product->id) : '' ?>
+    </div>
 </div>
