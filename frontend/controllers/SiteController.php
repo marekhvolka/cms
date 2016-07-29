0b65cb915a3d1fbe33aef219d59516097218a8e1
Marek <marek.hvolka@gmail.com>
2016-07-29 10:02:01 +0200

    Allowing to preview pages in portal, that dont have domain

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -78,7 +78,7 @@ class SiteController extends Controller
         ])
             ->one();
 
-        if (!$portal) {
+        if (!$portal || !Yii::$app->user->identity->portal->active) {
             $portal = Yii::$app->user->identity->portal;
         }
 
