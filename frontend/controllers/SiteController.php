2cae50cdc49c5e09d045ddf12c5f6c548b1b818b
Marek <marek.hvolka@gmail.com>
2016-08-16 23:59:10 +0200

    Comment problematic lines

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -71,8 +71,8 @@ class SiteController extends Controller
 
     public function actionIndex($url = null)
     {
-        if ($url[strlen($url) - 1] != '/')
-            return $this->redirect($url . '/', '301');
+        //if ($url[strlen($url) - 1] != '/')
+            //return $this->redirect('/' . $url . '/', '301');
 
         $identifiers = explode("/", strtolower($url));
 
