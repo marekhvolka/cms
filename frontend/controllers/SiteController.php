f516de5731ebe3e748adaad34ebfce4c37e7cfe5
Marek <marek.hvolka@gmail.com>
2016-08-17 10:32:47 +0200

    Fixing (hopefully?) routing bugs

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -71,9 +71,6 @@ class SiteController extends Controller
 
     public function actionIndex($url = null)
     {
-        //if ($url[strlen($url) - 1] != '/')
-            //return $this->redirect('/' . $url . '/', '301');
-
         $identifiers = explode("/", strtolower($url));
 
         $portal = Portal::find()->where([
